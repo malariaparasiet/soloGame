e59a1bab8801a6b6a021605d7213840bc04db15c
GeekieCoder <40068332+malariaparasiet@users.noreply.github.com>
2024-10-08 23:59:23 +0200

    De code gerewrite om classes te gebruiken
    shit is hella smexy DAMN

--- main.py
+++ main.py
@@ -1,103 +1,147 @@
 import pygame, sys, math, random
-from pygame.locals import QUIT
+from pygame.locals import *
 
+
+#pygame initializing
 pygame.init()
-screen = pygame.display.set_mode((1280, 720))
-pygame.display.set_caption('SoloGame')
-font = pygame.font.Font(None, 50)
+
+# Krijg informatie over de monitor zodat je automatisch fullscreen wordt gezet
+infoObject = pygame.display.Info()
+
+screen = pygame.display.set_mode((infoObject.current_w, infoObject.current_h), HWSURFACE | DOUBLEBUF )
+pygame.display.set_icon(pygame.image.load("graphics/playerIMG.png"))
+pygame.display.set_caption("SoloGame Koen Veldkamp H5")
 clock = pygame.time.Clock()
 
-background_surface = pygame.image.load("graphics/backgroundIMG.png").convert()
-background_image = pygame.transform.scale(background_surface, (1280, 720))
+# Maak class voor achtergrond
 
-player_ogSurface = pygame.image.load("graphics/playerIMG.png").convert_alpha()
-player = pygame.transform.scale(player_ogSurface, (62.6, 41.4))
-player_pos = screen.get_rect().center
-player_rect = player.get_rect(center=player_pos)
+class Background(object):
+    def __init__(self):
+        self.background_ogImg = pygame.image.load("graphics/backgroundIMG.png").convert()
 
-enemy_ogSurface = pygame.image.load("graphics/enemyIMG.png").convert_alpha()
-enemy = pygame.transform.scale(enemy_ogSurface, (62.6, 41.4))
+        self.background_image = pygame.transform.scale(self.background_ogImg, (infoObject.current_w, infoObject.current_h))
 
-zwaartekracht = 0
-game_actief = True
+    def draw(self):
+        screen.blit(self.background_image, (0,0))
 
-correction_angle = 0
+# Maak class voor bullets
 
-enemies = []
+class Bullet(object):
+    def __init__(self, x, y, velocity, angle):
+        self.x = x
+        self.y = y
+        self.angle = angle
+        self.velocity = velocity
 
-# Brainstorm:
-#   - een difficulty slider en die past dan de waarde vna de zombie gen aan met de modulo zodat er meer zombies komen per difficulty
-#   - Een score meter die met een multiplier
-#   - Eigen map met een beetje animatie
-#   - Eigen soundtrack (8 of 16-bit)
+    def debugShooting(self):
+        if pygame.mouse.get_pressed()[0]:
+            pygame.draw.circle(screen, "red", (player.rot_image_rect.x, player.rot_image_rect.y), 5)
 
-def shoot():
-    print("Shooting")
+# Maak class voor player
 
-def enemieGen():
+class Player(object):
+    def __init__(self, speed, health):
 
-    if pygame.time.get_ticks() % 20 == 0 and len(enemies) < 10:
-        enemyPosX = random.randint(0, 1280)
-        enemyPosY = random.randint(0, 720)
+        #position player to middle & player variables
+        self.x = infoObject.current_w / 2
+        self.y = infoObject.current_h / 2
+        self.speed = speed
+        self.health = health
+        self.correction_angle = 0
 
-        print(f"Generating an enemy on {enemyPosX}, {enemyPosY}")
+        #image magic because scaling n shi
+        self.oldImage = pygame.image.load("graphics/playerIMg.png")
+        self.scaledImage =  pygame.transform.scale(self.oldImage, (62.6, 41.4))
+        self.rectImage = self.scaledImage.get_rect(center=(self.x, self.y))
 
-        enemy_rect = enemy.get_rect(center=(enemyPosX, enemyPosY))
-        enemies.append(enemy_rect)  # Voeg de nieuwe vijand toe aan de lijst
+    #functie om movement mogelijk te maken
+    def movement(self):
+        self.keys = pygame.key.get_pressed()
+        if self.keys[pygame.K_d]:
+            self.rectImage.centerx += self.speed
+        if self.keys[pygame.K_a]:
+            self.rectImage.centerx -= self.speed
+        if self.keys[pygame.K_w]:
+            self.rectImage.centery -= self.speed
+        if self.keys[pygame.K_s]:
+            self.rectImage.centery += self.speed
+
+    #kijken of de speler niet buiten de game kan lopen
+    def checkIfInsideBoundry(self):
+        if self.rectImage.right >= infoObject.current_w:
+            self.rectImage.right = infoObject.current_w
+        if self.rectImage.left <= 0:
+            self.rectImage.left = 0
+        if self.rectImage.bottom >= infoObject.current_h:
+            self.rectImage.bottom = infoObject.current_h
+        if self.rectImage.top <= 0:
+            self.rectImage.top = 0
+
+    # wiskunde magie zodat de speler naar de muis kijkt
+    def playerLooksAtMouse(self):
+        mx, my = pygame.mouse.get_pos()
+        dx, dy = mx - self.rectImage.centerx, my - self.rectImage.centery
+        angle = math.degrees(math.atan2(-dy, dx)) - self.correction_angle
+
+        # draaien
+        rot_image = pygame.transform.rotate(self.scaledImage, angle)
+        self.rot_image_rect = rot_image.get_rect(center=self.rectImage.center)
+
+        screen.blit(rot_image, self.rot_image_rect.topleft)
+  
+# Maak class voor enemy
+
+class Enemy(object):
+    def __init__(self, health, spawnrate, speed):
+        self.health = health
+        self.spawnrate = spawnrate
+        self.speed = speed
+        self.oldImage = pygame.image.load("graphics/enemyIMG.png").convert_alpha()
+        self.scaledImage = pygame.transform.scale(self.oldImage, (62.6, 41.4))
+        self.enemyList = []
+
+    def GenerateEnemy(self):
+        # een modules operation voor om de spawnrate te bepalen en voor semi "random" spawntijden
+        if pygame.time.get_ticks() % self.spawnrate == 0 and len(self.enemyList) < 10:
+            
+            # random positie krijgen om een enemy te spawnen
+            self.posX = random.randint(0, 1280)
+            self.posY = random.randint(0, 720)
+
+            # debug
+            print(f"Generating an enemy on {self.posX}, {self.posY}")
+
+            # als die een enemy kan spawnen dan ook de rect aanmaken en toevoegen aan een lijst zodat de enemy op het scherm blijft
+            self.rectImage = self.scaledImage.get_rect(center=(self.posX, self.posY))
+            self.enemyList.append(self.rectImage)  # Voeg de nieuwe vijand toe aan de lijst
+
+    def draw(self):
+        # loopt over de lijst om de enemies elk frame gewoon te spawnen zodat ze blijven
+        for self.rectImage in self.enemyList:
+            screen.blit(self.scaledImage, self.rectImage)
+
+# classes callen
+background = Background()
+bullet = Bullet(0,0,0,0)
+player = Player(7.5, 100)
+enemy = Enemy(100, 20, 10)
 
 
-
-while True:
-    # Event handling
+running = True
+while running:
+    background.draw()
     for event in pygame.event.get():
-        if event.type == QUIT:
-            pygame.quit()
+        if event.type == pygame.QUIT:
             sys.exit()
 
-    # Movement van de speler
-    keys = pygame.key.get_pressed()
-    if keys[pygame.K_d]:
-        player_rect.centerx += 5
-    if keys[pygame.K_a]:
-        player_rect.centerx -= 5
-    if keys[pygame.K_w]:
-        player_rect.centery -= 5
-    if keys[pygame.K_s]:
-        player_rect.centery += 5
-    
-    # Schiet mechanisme met muis
-    if pygame.mouse.get_pressed()[0]:
-        shoot()
+    player.checkIfInsideBoundry()
+    player.playerLooksAtMouse()
+    player.movement()
 
-    if player_rect.right >= 1280:
-        player_rect.right = 1280
-    if player_rect.left <= 0:
-        player_rect.left = 0
-    if player_rect.bottom >= 720:
-        player_rect.bottom = 720
-    if player_rect.top <= 0:
-        player_rect.top = 0
+    enemy.GenerateEnemy()
+    enemy.draw()
 
-    # Zodat de speler altijd naar de muis kijkt
-    mx, my = pygame.mouse.get_pos()
-    dx, dy = mx - player_rect.centerx, my - player_rect.centery
-    angle = math.degrees(math.atan2(-dy, dx)) - correction_angle
+    bullet.debugShooting()
 
-    # draaien
-    rot_image = pygame.transform.rotate(player, angle)
-    rot_image_rect = rot_image.get_rect(center=player_rect.center)
-
-    screen.blit(background_image, (0, 0))
-    screen.blit(rot_image, rot_image_rect.topleft)
-
-    # Debug knop(pen)
-    if keys[pygame.K_q]:
-        enemieGen()
-    if keys[pygame.K_t] and len(enemies) > 0:
-        enemies.pop(0)
-
-    for enemy_rect in enemies:
-        screen.blit(enemy, enemy_rect)
-
-    pygame.display.update()
+    pygame.display.flip()
     clock.tick(60)
