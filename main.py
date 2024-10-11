2acac7bd0e28b9bfe83bc0a410ed3a85e1f820e7
Malariaparasiet <40068332+malariaparasiet@users.noreply.github.com>
2024-10-11 13:27:44 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
 import pygame, sys, math, random, logging
 from pygame.locals import *
 
+from enemy_class import Enemy
+from player_class import Player
+from bullet_class import Bullet
+from background_class import Background
+
+
 #initializing
 pygame.init()
 logging.basicConfig(level=logging.DEBUG, format="%(asctime)s -  %(levelname)s - %(message)s")
@@ -13,24 +19,10 @@ pygame.display.set_icon(pygame.image.load("graphics/playerIMG.png"))
 pygame.display.set_caption("SoloGame Koen Veldkamp H5")
 clock = pygame.time.Clock()
 
-# Maak class voor achtergrond
-
-class Background(object):
-    def __init__(self):
-        self.background_ogImg = pygame.image.load("graphics/backgroundIMG.png").convert()
-        self.background_image = pygame.transform.scale(self.background_ogImg, (infoObject.current_w, infoObject.current_h))
-        logging.info("Initialized background class!")
-
-    def draw(self):
-        screen.blit(self.background_image, (0,0))
-
 # Maak class voor bullets
 
 class Bullet(object):
-    def __init__(self, x, y, velocity, angle):
-        self.x = x
-        self.y = y
-        self.angle = angle
+    def __init__(self, velocity):
         self.velocity = velocity
         logging.info("Initialized bullet class!")
 
@@ -120,11 +112,6 @@ class Enemy(object):
             self.rectImage = self.scaledImage.get_rect(center=(self.posX, self.posY))
             self.enemyList.append(self.rectImage)  # Voeg de nieuwe vijand toe aan de lijst
 
-    def draw(self):
-        # loopt over de lijst om de enemies elk frame gewoon te spawnen zodat ze blijven
-        for self.rectImage in self.enemyList:
-            screen.blit(self.scaledImage, self.rectImage)
-
     def move_towards_player(self):
         for self.rectImage in self.enemyList:    
             #Vind de richtings vector tussen de zombies en de speler
@@ -142,11 +129,22 @@ class Enemy(object):
         for self.rectImage in self.enemyList:
             if self.rectImage.colliderect(player.rot_image_rect):
                 logging.debug(f"The list of enemies: {self.enemyList}")
+    
+    def look_at_player(self):
+        for self.rectImage in self.enemyList:
+            mx, my = player.rot_image_rect.x , player.rot_image_rect.y
+            dx, dy = mx - self.rectImage.centerx, my - self.rectImage.centery
+            angle = math.degrees(math.atan2(-dy, dx)) - 0
+
+            rot_image = pygame.transform.rotate(self.scaledImage, angle)
+            self.rot_image_rect = rot_image.get_rect(center=self.rectImage.center)
+
+            screen.blit(rot_image, self.rot_image_rect.topleft)
 
 
 # classes callen en gelijk zetten aan een variable
 background = Background()
-bullet = Bullet(0,0,0,0)
+bullet = Bullet(0)
 player = Player(7.5, 100)
 enemy = Enemy(100, 20, 6)
 logging.info("Classes gecalled en geregeld!")
@@ -164,9 +162,9 @@ while running:
     player.movement()
 
     enemy.GenerateEnemy()
-    enemy.draw()
     enemy.move_towards_player()
     enemy.check_if_shot()
+    enemy.look_at_player()
 
     bullet.debugShooting()
 
